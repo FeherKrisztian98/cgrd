3a65b1c31b46dbc1b0335b37dbdd04f5ada579df
Fehér Krisztián <feherkrisztian98@protonmail.com>
2024-11-11 20:40:07 +0100

    Support of notifications

---
+++ source/ENUM/NotificationType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ENUM;
+
+/**
+ *  Represents the types of notifications that can be displayed in the application.
+ *  This enum includes two types: SUCCESS and ERROR
+ */
+enum NotificationType
+{
+    case SUCCESS;
+
+    case ERROR;
+
+    /**
+     * Get the CSS class associated with the notification type
+     *
+     * @return string The CSS class for the notification type.
+     */
+    public function getCssClass(): string
+    {
+        return match ($this) {
+            self::ERROR => 'notification--error',
+            self::SUCCESS => 'notification--success',
+        };
+    }
+}
---
+++ source/HTTP/Notification.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace HTTP;
+
+use ENUM\NotificationType;
+use JsonSerializable;
+
+/**
+ * Represents a notification message
+ */
+class Notification implements JSONSerializable
+{
+    /**
+     * Notification constructor
+     *
+     * @param string $message The message content of the notification
+     * @param NotificationType $type The type of notification (defaults to SUCCESS)
+     */
+    public function __construct(protected string $message, protected NotificationType $type = NotificationType::SUCCESS)
+    {
+    }
+
+    /**
+     * Serializes the notification object to an array for JSON encoding.
+     *
+     * @return array
+     */
+    public function jsonSerialize(): array
+    {
+        return [
+            'type' => $this->type->getCssClass(),
+            'message' => $this->message,
+        ];
+    }
+}
